f9303f68b3fa6da63039872c16b17f33fd608823
EidorianAvi <reneavila1993@gmail.com>
2020-07-29 22:56:59 -0600

    Update and polish select shoe page

--- src/components/CurrentView.js
+++ src/components/CurrentView.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const CurrentView = ({currentView}) => {
-    return (
-        <div className="current-view">
-            <img src={currentView.image}/>
+const CurrentView = ({ currentView }) => {
+  return (
+    <div className="current-view">
+      <section>
+        <h2>{currentView.name}</h2>
+        <div className="current-view-info-box">
+          <ul className="current-view-info">
+            <li>{currentView.brand.toUpperCase()}</li>
+            <li>{currentView.colors}</li>
+            <li>${currentView.retail_price}</li>
+            <li>{currentView.gender.toUpperCase()}</li>
+            <li>{currentView.title}</li>
+          </ul>
         </div>
-    );
-}
+        <button>Add to Collection</button>
+      </section>
+      <div className="current-view-image">
+        <img src={currentView.image} />
+      </div>
+    </div>
+  );
+};
 
 export default CurrentView;
--- src/components/SelectPage.js
+++ src/components/SelectPage.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import SelectOption from './SelectOption';
-import CurrentView from './CurrentView';
+import React from "react";
+import SelectOption from "./SelectOption";
+import CurrentView from "./CurrentView";
 
+const SelectPage = ({ shoes, currentView, selectCurrentView }) => {
+  const renderOptions = () => {
+    return shoes.map((shoe) => (
+      <SelectOption
+        key={shoe.id}
+        {...shoe}
+        selectCurrentView={selectCurrentView}
+      />
+    ));
+  };
 
-
-
-const SelectPage = ({shoes, currentView, selectCurrentView}) => {
-
-    const renderOptions = () => {
-        return shoes.map(shoe => <SelectOption key={shoe.id} {...shoe} selectCurrentView={selectCurrentView}/>)
-    }
-
-    return (
-        <div className='select-page'>
-            <div className="select-options">
-                <p>View:</p>
-                {renderOptions()}
-            </div>
-            <div className="select-view">
-                <CurrentView currentView={currentView} />
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="select-page">
+      <div className="select-options">
+        <p>Select:</p>
+        {renderOptions()}
+      </div>
+      <div className="select-view">
+        <CurrentView currentView={currentView} />
+      </div>
+    </div>
+  );
+};
 
 export default SelectPage;
