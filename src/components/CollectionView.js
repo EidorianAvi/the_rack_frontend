4d2c0dd2c65208839bc923d906c7183f5e407b6d
EidorianAvi <reneavila1993@gmail.com>
2020-07-31 11:44:45 -0600

    Style collection view page

--- src/components/CollectionView.js
+++ src/components/CollectionView.js
@@ -1,36 +1,27 @@
 import React from "react";
 
 const CollectionView = ({ collectionView }) => {
+  const handleClick = (event) => {};
 
+  return (
+    <div className="collection-view">
+      <div className="current-view-image">
+        <img src={collectionView.image} alt="Oh no!" />
+      </div>
+      <div className="collection-view-info">
+        <h2>{collectionView.name}</h2>
 
-    const handleClick = (event) => {
-
-    }
-
-    // return(
-    //     <div>
-
-    //     </div>
-    // )
-    return (
-        <div className="current-view">
-            <div className="current-view-image">
-                <img src={collectionView.image} alt="Oh no!" />
-            </div>
-            <section>
-                <h2>{collectionView.name}</h2>
-                <div className="current-view-info-box">
-                    <ul className="current-view-info">
-                        <li>{collectionView.brand}</li>
-                        <li>{collectionView.colors}</li>
-                        <li>${collectionView.retail_price}</li>
-                        <li>{collectionView.gender}</li>
-                        <li>{collectionView.title}</li>
-                    </ul>
-                </div>
-            </section>
-        </div>
-    );
+        <ul >
+          <li>{collectionView.brand}</li>
+          <li>{collectionView.colors}</li>
+          <li>${collectionView.retail_price}</li>
+          <li>{collectionView.gender}</li>
+          <li>{collectionView.title}</li>
+        </ul>
+        <button onClick={handleClick}>Remove from collection</button>
+      </div>
+    </div>
+  );
 };
 
 export default CollectionView;
